b2b99aa41d4eaf239ef5f6bd534d52de9f8ddf6c
Stefan Midjich <swehack@gmail.com>
2016-11-02 11:42:58 +0100

    vRealize sends /alertId in the path
    Also can send /test when you setup the alert API.

--- api.py
+++ api.py
@@ -1,22 +1,32 @@
 # Proxy API for vRealize REST alerts
 
-# Makes debugging easier
-from pprint import pprint as pp
-
 import json
 import pkg_resources
-
-# Until pyvenv-3.4 is fixed on centos 7 support python 2.
+from uuid import UUID
 try:
     from configparser import RawConfigParser
 except ImportError:
     from ConfigParser import RawConfigParser
 
-from bottle import default_app, route, run, hook
+from bottle import default_app, run, Bottle
 from bottle import request, response, template, static_file
 
 from logger import Logger
 from vrealize import VRealizeAlert
+
+
+# Custom UUID route filter for bottle.py
+def uuid_filter(config):
+    # Should catch UUIDv4 type strings
+    regexp = r'[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}'
+
+    def to_python(match):
+        return UUID(match, version=4)
+
+    def to_url(uuid):
+        return str(uuid)
+
+    return regexp, to_python, to_url
 
 
 # Read configuration
@@ -27,18 +37,28 @@ config.read(['api_local.cfg', '/etc/vrealize-alerts/api.cfg'])
 # Setup logging, and a shorthand for future log messages
 log = Logger(config)
 
+app = Bottle()
+app.router.add_filter('uuid', uuid_filter)
+
 # Set a json content type for all responses
-@hook('after_request')
+@app.hook('after_request')
 def json_response():
     response.content_type = 'application/json'
 
 
-@route('/', method=['POST', 'PUT'])
-def index():
+@app.route('/test', method=['POST', 'PUT'])
+@app.route('/<alertid:uuid>', method=['POST', 'PUT'])
+def index(alertid=None):
     log.debug('Received alert from {client_ip}: {data}'.format(
         client_ip=request.remote_route[0],
         data=str(request.body)
     ))
+
+    # Most likely test alert
+    if not alertid:
+        return json.dumps({
+            'status': True
+        })
 
     # Process alert data
     try:
@@ -90,10 +110,10 @@ def index():
 
 
 if __name__ == '__main__':
-    run(
+    app.run(
         host=config.get('api', 'listen_host'),
         port=config.getint('api', 'listen_port')
     )
     debug(config.getbool('api', 'debug'))
 else:
-    application = default_app()
+    application = app.default_app()
