e48bcbb7e91653a2f28e3e05554a4cddadb79996
Stefan Midjich <swehack@gmail.com>
2016-11-02 09:33:46 +0100

    message object.

--- vrealize.py
+++ vrealize.py
@@ -1,3 +1,9 @@
+# vRealize alert message object to handle JSON and XML in a unified way.
+# Not been able to test XML yet so it's not implemented. Would be best to
+# use the builtin lxml.etree.ElementTree parser.
+
+import json
+
 class VRealizeAlert(object):
 
     def __init__(self, message, **kw):
@@ -16,8 +22,23 @@ class VRealizeAlert(object):
         
         self.startDate = message_object.get('startDate', None)
         self.criticality = message_object.get('criticality', None)
-        # TODO: Go on...
+        self.resourceId = message_object.get('resourceId', None)
+        self.alertId = message_object.get('alertId', None)
+        self.status = message_object.get('status', None)
+        self.subType = message_object.get('subType', None)
+        self.cancelDate = message_object.get('cancelDate', None)
+        self.resourceKind = message_object.get('resourceKind', None)
+        self.adapterKind = message_object.get('adapterKind', None)
+        self.type = message_object.get('type', None)
+        self.resourceName = message_object.get('resourceName', None)
+        self.updateDate = message_object.get('updateDate', None)
+        self.info = message_object.get('info', None)
 
 
     def read_json(self, message):
         return json.load(message)
+
+
+    # TODO:
+    def read_xml(self, message):
+        return False
